fc2318d2ba117da967e5ea6b7121a3dfc9bfca87
julianaml <jmora216@unicauca.edu.co>
2020-09-02 18:12:21 -0500

    Primera Version

---
+++ src/main/java/co/unicauca/agenciaviajes/negocio/ViajeIncentivo.java
@@ -0,0 +1,83 @@
+package co.unicauca.agenciaviajes.negocio;
+
+import java.util.Date;
+
+/**
+ *
+ * @author julia
+ */
+public class ViajeIncentivo extends Viaje {
+
+    /**
+     * Empresa que patrocina el viaje al empleado
+     */
+    private String empresa;
+    
+    //Constructor, getters and setters
+    public ViajeIncentivo(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada, String empresa) {
+        this.origen = origen;
+        this.destino = destino;
+        this.costo = costo;
+        this.fechaSalida = fechaSalida;
+        this.fechaLlegada = fechaLlegada;
+        this.empresa = empresa;
+    }
+    
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public Date getFechaLlegada() {
+        return fechaLlegada;
+    }
+
+    public void setFechaLlegada(Date fechaLlegada) {
+        this.fechaLlegada = fechaLlegada;
+    }
+
+    @Override
+    public String descripcion() {
+        return "Viaje incentivo que te envia la empresa " + empresa;
+    }
+
+    @Override
+    public String cualquierMetodo2() {
+        return "Método implementado en la clase hija viaje de incentivo";
+    }
+}
